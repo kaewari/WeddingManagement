b0b99b29a19d55ccee6aa1f96cccda6bb630bd3b
fenoriz <dev.tiennm@gmail.com>
2023-09-09 06:24:04 +0700

    damn git

---
+++ java-server/src/main/java/com/qltc/json/ChangedAttributeCheck.java
@@ -0,0 +1,13 @@
+package com.qltc.json;
+
+public class ChangedAttributeCheck {
+    
+    public static Object updateChangedAttribute(Object check, Object original, Class whatClass) {
+       return null;
+    }
+    
+    public static boolean ChangedObjectCheck(Object check, Object original) {
+        return false;
+    }
+    
+}
