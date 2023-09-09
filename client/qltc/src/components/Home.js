82b273c56657d6d00ea8c70ce4626f0e2bc45514
kaewari <sonhoang236@gmail.com>
2023-09-09 20:09:31 +0700

    update

--- client/qltc/src/components/Home.js
+++ client/qltc/src/components/Home.js
@@ -1,10 +1,12 @@
-import { Col, Image} from "react-bootstrap"
+import { Col, Image, Row} from "react-bootstrap"
 const Home = () =>
 {
     return (
-        <Col>
-            <Image src={"https://res.cloudinary.com/dt8p4xhzz/image/upload/v1694261175/tiec%20cuoi/tiec-cuoi-1_klwjom.jpg"} />
-        </Col>
+        <Row >
+            <Col xs={12}>
+                <Image src={"https://res.cloudinary.com/dt8p4xhzz/image/upload/v1694261175/tiec%20cuoi/tiec-cuoi-1_klwjom.jpg"} />
+            </Col>
+        </Row>
     )
 }
 export default Home;
